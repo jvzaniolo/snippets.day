6951c3e524e45b3ef65dfce3d3a675d9e166cd58
João Vitor <54036572+jvzaniolo@users.noreply.github.com>
2022-02-26 11:22:21 -0400

    fix: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
     <header className="z-20 flex h-12 items-center px-4 py-2 shadow-md dark:bg-moon-700 dark:shadow-xl">
       <div className="container-lg flex justify-between">
         <Link href="/">
-          <a className="rounded px-2 py-1 text-lg font-semibold tracking-wide transition-colors active:bg-moon-100 dark:active:bg-moon-600 lg:hover:bg-moon-100 lg:dark:hover:bg-moon-600">
+          <a className="rounded px-2 py-1 text-lg font-medium tracking-wide transition-colors active:bg-moon-100 dark:active:bg-moon-600 lg:hover:bg-moon-100 lg:dark:hover:bg-moon-600">
             🚀 Snippets
           </a>
         </Link>
@@ -23,25 +23,32 @@ const Header = () => {
         <div className="flex items-center">
           <button
             type="button"
-            aria-label="Toggle theme"
             onClick={toggleTheme}
-            className="button !py-2.5"
+            aria-label="Toggle theme"
+            className="flex h-9 w-9 items-center justify-center rounded transition-colors active:bg-moon-100 dark:active:bg-moon-600 lg:hover:bg-moon-100 lg:dark:hover:bg-moon-600"
           >
             {icon}
           </button>
 
           {session?.user ? (
-            <button onClick={() => supabase.auth.signOut()} className="button-primary ml-3">
+            <button
+              onClick={() => supabase.auth.signOut()}
+              className="ml-2 flex h-9 items-center rounded bg-gradient-to-br from-pink-500 via-orange-500 to-yellow-500 px-2 text-white transition-shadow hover:shadow-lg"
+            >
               Logout
             </button>
           ) : (
             <>
               <Link href="/login" passHref>
-                <a className="button ml-1">Login</a>
+                <a className="flex h-9 items-center rounded px-2 transition-colors active:bg-moon-100 dark:active:bg-moon-600 lg:hover:bg-moon-100 lg:dark:hover:bg-moon-600">
+                  Login
+                </a>
               </Link>
 
               <Link href="/sign-up" passHref>
-                <button className="button-primary ml-3">Create Account</button>
+                <button className="ml-2 flex h-9 items-center rounded bg-gradient-to-br from-pink-500 via-orange-500 to-yellow-500 px-2 text-white transition-shadow hover:shadow-lg">
+                  Create Account
+                </button>
               </Link>
             </>
           )}
